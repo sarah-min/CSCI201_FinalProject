240be07cbac43b4f0c55b27a2ba1d03448d46124
Siyuan Tang <siyuanta5@gmail.com>
2025-04-03 16:52:35 -0700

    Initialize Backend

---
+++ Spotify-matcher/src/main/java/com/cs201/Spotify_matcher/SpotifyMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.cs201.Spotify_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyMatcherApplication.class, args);
+	}
+
+}
---
+++ Spotify-matcher/src/test/java/com/cs201/Spotify_matcher/SpotifyMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs201.Spotify_matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
