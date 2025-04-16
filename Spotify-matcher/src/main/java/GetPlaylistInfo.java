c8c2ca426106b5feeeac2a64daf78a31540af3b0
Sarah <sarah.yankun@gmail.com>
2025-04-16 16:33:44 -0700

    dynamic web project test

--- Spotify-matcher/src/main/java/GetPlaylistInfo.java
+++ Spotify-matcher/src/main/java/GetPlaylistInfo.java
@@ -8,14 +8,7 @@ public class GetPlaylistInfo {
     Map<String, Map<String, Integer>> trackFeatures;
 
     // user enters last.fm link
-    // http call to get the info from front end input??
-
-    // user enters link, get Spotify ID from link;
-    public void parseLink(String link) {
-        int s = link.indexOf("playlist") + 9;
-        int e = link.indexOf("?", s);
-        this.id = link.substring(s+9, e);
-    }
+    // http call to get the info from front end input
 
     // use Last.fm track.getTags to make call to Last.fm API to get playlist information
     // get tags from song
